36b9f6d74781fe630ea1317e94db652c5148a3ce
antareza <oneakun08@gmail.com>
2024-01-11 09:43:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/main_page.dart';
+
+void main() {
+  MyApp app = const MyApp();
+
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
